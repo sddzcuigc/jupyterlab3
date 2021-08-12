a8ec37c71ae7e719e2295b971531f90c683a665b
sddzcuigc <85976753+sddzcuigc@users.noreply.github.com>
2021-08-12 23:42:25 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # start 1
-/opt/pierced/linux/ding -config=/opt/pierced/linux/ding.cfg -subdomain=playkof 8888 &
+"/opt/pierced/linux/ding -config=/opt/pierced/linux/ding.cfg -subdomain=playkof 8888" &
 # start 2
-jupyter lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --allow-root --no-browser &
+"jupyter lab --notebook-dir=/opt/notebooks --ip='*' --port=8888 --allow-root --no-browser" &
 
 # just keep this script running
 while [[ true ]]; do
